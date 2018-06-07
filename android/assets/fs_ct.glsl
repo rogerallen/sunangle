116cea3478d9ce2c1df3d175b925beb8383902cb
Roger Allen <rallen@gmail.com>
2018-06-06 21:52:07 -0700

    Rename shaders.

--- android/assets/fs_ct.glsl
+++ android/assets/fs_ct.glsl
@@ -9,6 +9,5 @@ varying vec2 v_texCoord;
 uniform sampler2D s_texture;
 
 void main() {
-    gl_FragColor = texture2D( s_texture, v_texCoord );
-    //gl_FragColor = vec4(v_texCoord.xyxy);//v_color);
+    gl_FragColor = v_color * texture2D( s_texture, v_texCoord );
 }
